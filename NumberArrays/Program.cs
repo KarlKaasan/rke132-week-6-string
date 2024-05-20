ba884b74fa5eaf2e353b51c0a44bc79bfbd1aab4
KarlKaasan <Karl.Kaasan@tktk.ee>
2024-05-20 12:44:55 +0300

    new app

---
+++ NumberArrays/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+
+int[] number = new int[10];
+Random rnd = new Random();
+
+int sum = 0;
+
+for (int i = 0; i < number.Length; i++)
+{
+    number[i] = rnd.Next(1, number.Lenght + 1);
+    sum = sum + NumberStyles[i];
+}
+
+foreach (int i in number)
+{
+    Console.WriteLine(number);
+}
+
+Console.WriteLine($"Total: {sum}");
